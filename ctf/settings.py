c897141ca2a6edf15f93497f9f86bac197f28b43
armanheids <armanheids@gmail.com>
2024-01-29 03:28:26 +0330

    change .gitignore

--- ctf/settings.py
+++ ctf/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-on0o@ckw6blrw^une2u%lqix7c&$32gn$oz-##@82f_u40af2z"
+SECRET_KEY = "ctf"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
